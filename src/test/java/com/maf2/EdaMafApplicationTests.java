226279a0428575cf3fe33623a456c1e6f7b9cc81
MartinFiorde <fiorde_90@hotmail.com>
2022-05-26 11:59:40 -0300

    errata. coverage 74%, reflected now correctly in documentation

--- src/test/java/com/maf2/EdaMafApplicationTests.java
+++ src/test/java/com/maf2/EdaMafApplicationTests.java
@@ -3,6 +3,8 @@ package com.maf2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Total coverage from all testable classes
+// Tests: 26 - Testable methods: 35 - Coverage: 74%
 @SpringBootTest
 class EdaMafApplicationTests {
 
